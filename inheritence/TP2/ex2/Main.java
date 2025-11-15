e3d3f9abcaf4960ba98e99f9b0588232f094d7c6
mustapha <mostaphaelghazal102030@gmail.com>
2025-11-15 22:09:36 +0100

    modification personne

--- inheritence/TP2/ex2/Main.java
+++ inheritence/TP2/ex2/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        secretaire s= new secretaire("amina", 2000, "safi",7000);
+        persone s= new secretaire("amina", 2000, "safi",7000);
         s.afficher();
-        enseignant e=new enseignant("youssef",1999,"cs",9000,"info");
+        persone e=new enseignant("youssef",1999,"cs",9000,"info");
         e.afficher();
-        etudiantR etr=new etudiantR("ali",2001,"cs",15.5);
+        persone etr=new etudiantR("ali",2001,"cs",15.5);
         etr.afficher();
-        etudiantT ett=new etudiantT("hassan",2002,"is",16.5,"ueu");
+        persone ett=new etudiantT("hassan",2002,"is",16.5,"ueu");
         ett.afficher();                
         ET.afficherTotalEtudiants();
         System.out.println("Nombre total de ESTIENT: " + persone.getNombrePersones());
